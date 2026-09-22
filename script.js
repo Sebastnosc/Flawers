4060a7f2647f3db53279e228d388b792ee7c562d
Sebastnosc <gohanbalz@gmail.com>
2026-09-22 14:33:55 -0500

    Fix formatting and restore onload function

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
 onload = () => {
     document.body.classList.remove("container");
   };
-const CODIGO_CORRECTO = "150325"; // Tu clave aquí
-
-function verificarCodigo() {
-  const input = document.getElementById("codeInput").value;
-  const errorMsg = document.getElementById("errorMsg");
-  const overlay = document.getElementById("loginOverlay");
-
-  if (input === CODIGO_CORRECTO) {
-    errorMsg.style.display = "none";
-    overlay.classList.add("hidden");
-  } else {
-    errorMsg.style.display = "block";
-  }
-}
-
-document.getElementById("codeInput").addEventListener("keypress", function(event) {
-  if (event.key === "Enter") {
-    verificarCodigo();
-  }
-});
